924c6f3a9b20c951ad8067d8cac86513275866e9
Daynosolro <149596377+daynosolro@users.noreply.github.com>
2024-10-05 14:39:44 -0300

    Add files via upload

---
+++ storage/framework/views/7c0e94c18d4e5abbceac068228fbc056.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home Page</title>
+    
+</head>
+<body>
+    <div class="navbar">
+        <a href="<?php echo e(route('home')); ?>">Home</a>
+        <a href="<?php echo e(route('salones_disponibles', ['zona' => 'Ituzaingo'])); ?>">Salones Disponibles</a>
+        <a href="<?php echo e(route('contacto', ['numero' => 0])); ?>">Contacto</a>
+    </div>
+
+    <div class="content">
+        <h1>Bienvenido</h1>
+        <p>Este es el contenido de la página Home.</p>
+    </div>
+</body>
+</html>
+
+<style>
+        
+        .navbar {
+            background-color: #333;
+            overflow: hidden;
+        }
+
+        .navbar a {
+            float: left;
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 20px;
+            text-decoration: none;
+        }
+
+        .navbar a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+        .content {
+            padding: 20px;
+        }
+    </style>
+<?php /**PATH C:\xampp\htdocs\Salon_fiesta\resources\views/home.blade.php ENDPATH**/ ?>
